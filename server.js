4e83e6a34fb86094114b74bafe98d2246b43999e
Madreeban0 <madreeban277@gmail.com>
2025-02-12 09:41:44 +0530

    Connected mongodb

--- server.js
+++ server.js
@@ -1,38 +1,35 @@
-
-
-
 require('dotenv').config(); 
-
-const express = require('express');
 const mongoose = require('mongoose');
-
+const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
+const dotenv =require("dotenv");
+const connectDatabase = require('./database');
+dotenv.config();
 
-const mongoURI = process.env.DB_URL;
-if (!mongoURI) {
-  console.error("Error: Database URI is not defined. Check your .env file.");
-  process.exit(1);
-}
-
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("Connected to the database"))
-.catch(err => {
-  console.error("Database connection failed:", err);
-  process.exit(1);
-});
-
-app.get('/ping', (req, res)=>
-    {
-        res.send('Pong!');}
-    );
+connectDatabase();
 app.get('/', (req, res) => {
-  res.send("Server is running");
+    let dbStatus;
+    switch (mongoose.connection.readyState) {
+        case 0:
+            dbStatus = 'Disconnected';
+            break;
+        case 1:
+            dbStatus = 'Connected';
+            break;
+        case 2:
+            dbStatus = 'Connecting';
+            break;
+        case 3:
+            dbStatus = 'Disconnecting';
+            break;
+        default:
+            dbStatus = 'Unknown State';
+    }
+    res.json({ status: dbStatus });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
 });
